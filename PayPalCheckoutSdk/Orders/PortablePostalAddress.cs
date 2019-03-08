9170c40dd80f9d940c713479860769493af5bcac
pengli <pengli@paypal.com>
2019-03-08 10:17:38 -0800

    add PortablePostalAddress

---
+++ PayPalCheckoutSdk/Orders/PortablePostalAddress.cs
@@ -0,0 +1,86 @@
+﻿// This class was generated on Fri, 15 Feb 2019 15:03:18 PST by version 0.1.0-dev+8fcb5f of Braintree SDK Generator
+// PortablePostalAddress(MediumGrained).cs
+// @version 0.1.0-dev+8fcb5f
+// @type object
+// @data H4sIAAAAAAAC/8xY32/bOBJ+v79i4HtpAFlqkt29Q97SFNvd7bZbNGnvoVfEI3FsDUKR6pCy6x7ufz+QlGwrtvvjtgj2zSaHP+abb2Y+6j+Tm3VLk4vJKyseS03wyjqPGi6VEnIOHr0gxV0zfSbIhtTJJJu8ReFg+hKbsHKSTZ7TevvnKblKuPVszeRiclMTtMPebDyJwTCFGtp0EqaTcniBrQNv4V1/9lvUrKLxC/Ko0OP7R7X3rbsoigX7uivzyjbFwtqFJj79pyk0l/1ubNrOFyu+4+LobieARsEvNy9+hx/zU3h32Xk7Z63ZLGBupYHKGi9WuwvwNQF23la2aTV5AvReuOw8ba+0Wq3y1XluZVHcvC5q3+gfTwtH1TTs5fIw8HfcHhGHp8MRU1/TdPeE6eaEk3ySTS5FcJ1C9TibvCZUfxi9nlzMUTsKAx86FlKbgVdiWxLP5CYX7zZBHoL6lDyydvux7NG7VRuDbWj35/YjbayZbqKNSnEf6n4t9GvB1+gBhcDZhjw35MAQKVIBeIgYMJqKMhB2dxlYAetrEnAVGRS2DlY1CcGcDU0XgZubM9gEZGOkoeFF7aGkfvccXlq/peOKfR0Oa6wBX7MoaFH8OtLCtmTA2U4qyuE1qc4oNH5YEk8mrVz+7+7x4/OqlCL+oJ+tAH3EEMMMZgNkw4n5MKDZ0O3pDNhB5zrUeg0YblJyyg6w8+3qHrTceSHyt6ZrSpJZBrNhABuaZfHWw5BftzT7frxxXtgs9ulSdqwVm0W8wogs92fGVLmEMKxA2yq62xNCqBVyZLyLCdcKNewohzcbiIZt4/rAigQGRNJoTdUA3mDoUsRwCPN4XQ6jeKVYVlZRCuaV4JIM/GI7l0aqYjv5Z9E1ndb/zb4IsSLNS5L1rSNZcjVG+cDkfk4ORtAb5bHEg53PuSIo7ccMSlz0iMRUa3fmA2AP5OoOnUdejsf3HUzz6abwW+c8zJ5KJ+sZsOl/wu9o/nxCfKMfEdCDngwzx31J9HzQ+4aacei2/fjRu4b5e4kU0qajDErbaVqiqAzEoorkoo8hyd0K1w/lXlfeDuVg7N94Yt/BOYvzUyuKBMh49msoSdsVYF+/NuXIyqaWHSxlriunB8qZY7PQtN0m1KrQyb6hmOVwhSZ0OIS5Rp+B81bWGcy1tRJgt02EHUNra8j474L6+6/AfdzrDkqJzdQR7CEYBASi/hp04ohq2waQ6Hhvenb6j3PYLQEwOBeVRpCBIbSSmv5WeMS/QXtAVVN1F9RpKCxBrgWtEe4z7/RWvD4MlUe4nR2H9OxwvlJljfoKTF3HnkakediCNPLl/Lib5wfdTErugJcZ8HwjBMf1aqhlSX1HjwM9ngh+Yp2BYumz0dPHkGNdVQM6mBn66MOT5V+oGxQ/S6kGGo1qUO5CA0IDvxrFaB6cKw2bWxTCveQbTewDWPOippB8tCQdS5fiJbvgfl+cupAxGaxqruqxhm3FLjkKd+fRU8Tj1+s/puenP/00PQubDXvFEDSYgO6fgoNUuZ/EV5ezmJDGephdoea5FcM4y+Et6tho1ttbsbtIYq3TSamlf5rTvzfPc7hM1utevRfD3D3L62AZ/fi84RUaVBiMB/c/b/8btmiSOc2p8p18YcH1iv0nkkCrsOwOjbfm8JJi8PrBWXZ2jGVnB/R/xX6dgbcrEymyZK1xQTlcN6g1SWiiJryANptEMt6ezh4+e86P+XV+wK+QLUEJ6Oif68pOygwM8aIurdTWJhGkOBxc+S86fDZw/Eu0TqUqh+v+yBJZxMbDdk//PM9ioYp7bJy4d9ut1B69s9nF3ES9wrUDXCLr+MIuOx+K65H9oBqkS3qfBCggOP9Xo/YPxyjww+GvIKNwr6L+/Zagn8+C+Szo0wG2r6VB+twWi6mz4oOoDE/WWGSfdSTGUZI6DZo1/Cxkqho8ibC3wuS2va2fe9YxGvw8bZJeGz7EkILoUfAWPS8pNRIXmsdVzeavEd2+XdwGdEbRvTexH913fmWnVY2ClafY3SB2t9OI9PtHhbKVK9h4WkjMjiLJh0LI+aLffhpsXXGSXguswvNizpReC71NIIHQgjelvtS2uvvQWU+7wDkv1izSyEvrqedHsTsONzvbbgkhhB6eCEdNy27vA8izJ3ufPTZ9+L7tm+cHbIM0IAW9Yva2nSZRoWwTjgx1xMWrRBj6C4YXEsUFs6uz2f61I4lgZUWrFfdjQbihxJrTmf6jqiYFrXBF8OjqzasTaMjXISFLNHdQxayMol+sc9MyPfO8oHEYtd7wba+4D/u3ENRL9//xMyXxPj3H44e+tcfkD/M7Ai2A+YnbhGGQVh86XqIOz0G4WbdcRfkmu0+jBHugZP/o3Nk5qlwz2uWaCN7tmGy/i5PJV3zHLYXeYmWRvsq/2vpx8p2epH/7HwAAAP//
+// DO NOT EDIT
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+
+
+namespace PayPalCheckoutSdk.Orders
+{
+    /// <summary>
+    /// The portable international postal address. Maps to [AddressValidationMetadata](https://github.com/googlei18n/libaddressinput/wiki/AddressValidationMetadata) and HTML 5.1 [Autofilling form controls: the autocomplete attribute](https://www.w3.org/TR/html51/sec-forms.html#autofilling-form-controls-the-autocomplete-attribute).
+    /// </summary>
+    [DataContract]
+    public class PortablePostalAddress
+    {
+        /// <summary>
+        /// Required default constructor
+        /// </summary>
+        public PortablePostalAddress() {}
+
+/// <summary>
+/// The non-portable additional address details that are sometimes needed for compliance, risk, or other scenarios where fine-grain address information might be needed. Not portable with common third party and open source. Redundant with core fields.<br/>For example, `address_portable.address_line_1` is usually a combination of `address_details.street_number`, `street_name`, and `street_type`.
+/// </summary>
+[DataMember(Name = "address_details", EmitDefaultValue = false)]
+public AddressDetails AddressDetails;
+
+/// <summary>
+/// The first line of the address. For example, number or street. For example, `173 Drury Lane`. Required for data entry and compliance and risk checks. Must contain the full address.
+/// </summary>
+[DataMember(Name = "address_line_1", EmitDefaultValue = false)]
+public string AddressLine1;
+
+/// <summary>
+/// The second line of the address. For example, suite or apartment number.
+/// </summary>
+[DataMember(Name = "address_line_2", EmitDefaultValue = false)]
+public string AddressLine2;
+
+/// <summary>
+/// The third line of the address, if needed. For example, a street complement for Brazil, direction text, such as `next to Walmart`, or a landmark in an Indian address.
+/// </summary>
+[DataMember(Name = "address_line_3", EmitDefaultValue = false)]
+public string AddressLine3;
+
+/// <summary>
+/// The highest level sub-division in a country, which is usually a province, state, or ISO-3166-2 subdivision. Format for postal delivery. For example, `CA` and not `California`. Value, by country, is:<ul><li>UK. A county.</li><li>US. A state.</li><li>Canada. A province.</li><li>Japan. A prefecture.</li><li>Switzerland. A kanton.</li></ul>
+/// </summary>
+[DataMember(Name = "admin_area_1", EmitDefaultValue = false)]
+public string AdminArea1;
+
+/// <summary>
+/// A city, town, or village. Smaller than `admin_area_level_1`.
+/// </summary>
+[DataMember(Name = "admin_area_2", EmitDefaultValue = false)]
+public string AdminArea2;
+
+/// <summary>
+/// A sub-locality, suburb, neighborhood, or district. Smaller than `admin_area_level_2`. Value is:<ul><li>Brazil. Suburb, bairro, or neighborhood.</li><li>India. Sub-locality or district. Street name information is not always available but a sub-locality or district can be a very small area.</li></ul>
+/// </summary>
+[DataMember(Name = "admin_area_3", EmitDefaultValue = false)]
+public string AdminArea3;
+
+/// <summary>
+/// The neighborhood, ward, or district. Smaller than `admin_area_level_3` or `sub_locality`. Value is:<ul><li>The postal sorting code for Guernsey and many French territories, such as French Guiana.</li><li>The fine-grained administrative levels in China.</li></ul>
+/// </summary>
+[DataMember(Name = "admin_area_4", EmitDefaultValue = false)]
+public string AdminArea4;
+
+/// <summary>
+/// REQUIRED
+/// The [two-character ISO 3166-1 code](/docs/integration/direct/rest/country-codes/) that identifies the country or region.<blockquote><strong>Note:</strong> The country code for Great Britain is <code>GB</code> and not <code>UK</code> as used in the top-level domain names for that country. Use the `C2` country code for China worldwide for comparable uncontrolled price (CUP) method, bank card, and cross-border transactions.</blockquote>
+/// </summary>
+[DataMember(Name = "country_code", EmitDefaultValue = false)]
+public string CountryCode;
+
+/// <summary>
+/// The postal code, which is the zip code or equivalent. Typically required for countries with a postal code or an equivalent. See [postal code](https://en.wikipedia.org/wiki/Postal_code).
+/// </summary>
+[DataMember(Name = "postal_code", EmitDefaultValue = false)]
+public string PostalCode;
+    }
+}
+
